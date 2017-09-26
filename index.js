a8a23bb4b09e6d91809c01cc41f18dac2c013ef2
NicholasGalarza <nicogala93@gmail.com>
2017-09-26 15:54:53 -0400

    class methods done

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ var Page = models.Page;
 var User = models.User;
 var app = require('./app');
 
-User.sync()
+User.sync({force: true})
     .then(function () {
-        return Page.sync();
+        return Page.sync({force: true});
     })
     .then(function () {
         app.listen(3001, function () {
             console.log('Server is listening on port 3001!');
         });
     });
-
--- test/page.model.spec.js
+++ test/page.model.spec.js
@@ -1,6 +1,9 @@
 const expect = require('chai').expect;
 const {Page} = require('../models');
 const marked = require('marked');
+const chai = require("chai");
+chai.should();
+chai.use(require('chai-things'));
 
 describe('Page model', function () {
     let page;
@@ -25,14 +28,49 @@ describe('Page model', function () {
 
   describe('Class methods', function () {
     describe('findByTag', function () {
-      it('gets pages with the search tag');
-      it('does not get pages without the search tag');
+      it('gets pages with the search tag', () => {
+        page.title = 'PETS';
+        page.content = "Having pets is great but it also sucks...";
+        page.tags = ['cats', 'dogs', 'goldfish'];
+
+        Page.findByTag('cat'); //.then();
+      });
+      it('does not get pages without the search tag', (done) => {
+        Page.findByTag('foo')
+        .then( pages => {
+          expect(pages).to.have.lengthOf(0);
+          console.log(pages);
+          done();
+          //done();
+        })
+        .catch(function(err) {
+          console.log(err);
+          done();
+        });
+      });
     });
   });
 
-  describe('Instance methods', function () {
-    describe('findSimilar', function () {
-      it('never gets itself');
+  xdescribe('Instance methods', function () {
+    describe('findSimilar', function (done) {
+      let page1 = Page.create({
+        tags: ['cats', 'dogs', 'goldfish']
+      });
+
+      let page2 = Page.create({
+        tags: ['potatoes', 'celery', 'onions']
+      });
+
+      let page3 = Page.create({
+        tags: ['foo', 'foo', 'foo']
+      });
+
+      it('never gets itself', () => {
+        page.tags = ['cats', 'dogs', 'goldfish'];
+        page.findSimilar();
+        done();
+
+      });
       it('gets other pages with any common tags');
       it('does not get other pages without any common tags');
     });
